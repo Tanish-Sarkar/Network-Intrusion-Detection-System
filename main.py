9dd69e5c9dfbb8cb7f80b4fffbb33a2f526cf47d
Tanish-Sarkar <tanishnitch@gmail.com>
2025-07-31 18:32:26 +0530

    Kmeans check done

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from src.data_loader import load_data
 from src.preprocessing import preprocess_data, split_data
-from src.model import train_model, save_model
-from src.evaluation import evaluate_model, plot_confusion_matrix
-from src.utils import save_metrics
+# from src.model import train_model, save_model
+# from src.utils import save_metrics
 import os
 
 def main():
     current_dir = os.path.dirname(__file__)  
-    file_path = os.path.join(current_dir, './data/processed/Cleaned_Telco.csv')
+    file_path = os.path.join(current_dir, './data/raw/Train_data.csv')
 
     df = load_data(file_path)
     print("✅ Data Loaded Successfully...")
@@ -16,22 +15,22 @@ def main():
     X, y = preprocess_data(df)
     print("✅ Data Preprocessed...")
 
-    X_train, X_test, y_train, y_test = split_data(X, y)
-    print("✅ Data Split into Train/Test")
+    # X_train, X_test, y_train, y_test = split_data(X, y)
+    # print("✅ Data Split into Train/Test")
 
-    model = train_model(X_train, y_train)
-    print("✅ Model Trained...")
+    # model = train_model(X_train, y_train)
+    # print("✅ Model Trained...")
 
-    metrics = evaluate_model(model, X_test, y_test)
-    print("✅ Model Evaluated...")
+    # metrics = evaluate_model(model, X_test, y_test)
+    # print("✅ Model Evaluated...")
 
-    print("📊 Plotting Confusion Matrix...")
-    y_pred = model.predict(X_test)
-    plot_confusion_matrix(y_test, y_pred)
+    # print("📊 Plotting Confusion Matrix...")
+    # y_pred = model.predict(X_test)
+    # plot_confusion_matrix(y_test, y_pred)
 
-    print("💾 Saving Model and Metrics...")
-    save_model(model, "outputs/LogisticModel.joblib")
-    save_metrics(metrics)
+    # print("💾 Saving Model and Metrics...")
+    # save_model(model, "outputs/LogisticModel.joblib")
+    # save_metrics(metrics)
 
 if __name__ == "__main__":
     main()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,16 +8,22 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     if 'id' in df.columns:
         df.drop('customerID', axis=1, inplace=True)
 
-    cat_cols = df.select_dtypes(include='objects').columns
+    cat_cols = df.select_dtypes(include='object').columns
     le = LabelEncoder()
     for col in cat_cols:
         df[col] = le.fit_transform(df[col].astype(str))
 
     df.fillna(df.median(numeric_only=True), inplace=True)
 
-    X = df.drop('')
+    X = df.drop('class', axis=1)
+    y = df['class'] 
 
-    return X, y
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    df.to_csv('data/processed/processed_data.csv', index=False)
+    return X_scaled, y
+
 
 def split_data(X, y):
     return train_test_split(X, y, test_size=0.2, random_state=42)
